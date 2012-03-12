bf7e6e3f3f62fa0d40cb0e1b6971120f1c52be2a
Asaka Kage <arcticfox9902@gmail.com>
2012-03-12 17:05:06 +0800

    first commit of Spoon-Knife
    Signed-off-by: Asaka Kage <arcticfox9902@gmail.com>

---
+++ src/com/arcticfox/test/SimpleCompute.java
@@ -0,0 +1,10 @@
+package com.arcticfox.test;
+
+public class SimpleCompute {
+	public String toString(){
+		return SimpleCompute.class.getName();
+	}
+	public static void main(String[] args) {
+		System.out.println(1+2);
+	}
+}
---
+++ src/com/arcticfox/test/SimpleSub.java
@@ -0,0 +1,7 @@
+package com.arcticfox.test;
+
+public class SimpleSub {
+	public static void main(String[] args) {
+		System.out.println(1+1);
+	}
+}
